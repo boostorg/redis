548e3d4cb6919969f0219aa82db5d6951ea88346
Marcelo Zimbres <mzimbres@gmail.com>
2023-10-07 16:44:17 +0200

    Updates the copywrite notice.

--- include/boost/redis/ignore.hpp
+++ include/boost/redis/ignore.hpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2018-2022 Marcelo Zimbres Silva (mzimbres@gmail.com)
+/* Copyright (c) 2018-2023 Marcelo Zimbres Silva (mzimbres@gmail.com)
  *
  * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE.txt)
