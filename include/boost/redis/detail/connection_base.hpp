86015cbdde0f96cc7907939cd781eab86cd257fa
Marcelo Zimbres <mzimbres@gmail.com>
2024-10-12 22:15:06 +0200

    Disentangles resolve and connect from run operations.

--- include/boost/redis/detail/connection_base.hpp
+++ include/boost/redis/detail/connection_base.hpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2023 Marcelo Zimbres Silva (mzimbres@gmail.com)
+/* Copyright (c) 2018-2024 Marcelo Zimbres Silva (mzimbres@gmail.com)
  *
  * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE.txt)
@@ -705,7 +705,7 @@ private:
    template <class, class> friend struct writer_op;
    template <class, class> friend struct run_op;
    template <class> friend struct exec_op;
-   template <class, class, class> friend struct run_all_op;
+   template <class, class, class> friend struct runner_op;
 
    void cancel_push_requests()
    {
