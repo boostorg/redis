0a7a0e16d13d439dc3ae5e8677eeedc36e226153
Ruben Perez <rubenperez038@gmail.com>
2025-05-14 20:06:18 +0200

    adjust copyright of new files

--- include/boost/redis/detail/coroutine.hpp
+++ include/boost/redis/detail/coroutine.hpp
@@ -1,5 +1,6 @@
 //
-// Copyright (c) 2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+// Copyright (c) 2025 Marcelo Zimbres Silva (mzimbres@gmail.com),
+// Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
