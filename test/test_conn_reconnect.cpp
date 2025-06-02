398de414d2b998dc12c901aa6f651bd7b7ea73ea
Ruben Perez <rubenperez038@gmail.com>
2025-06-02 19:01:11 +0200

    Fixed the reconnection test

--- test/test_conn_reconnect.cpp
+++ test/test_conn_reconnect.cpp
@@ -46,7 +46,7 @@ net::awaitable<void> test_reconnect_impl()
    regular_req.get_config().cancel_on_connection_lost = false;
 
    auto conn = std::make_shared<connection>(ex);
-   boost::redis::config cfg;
+   auto cfg = make_test_config();
    cfg.reconnect_wait_interval = 100ms;  // make the test run faster
    run(conn, std::move(cfg));
 
@@ -59,13 +59,10 @@ net::awaitable<void> test_reconnect_impl()
          co_await conn->async_exec(quit_req, ignore, net::redirect_error(ec));
          BOOST_TEST(ec == error_code());
 
-         // TODO: this currently fails sometimes, probably due to
-         // https://github.com/boostorg/redis/issues/255.
-         // Uncomment when the issue is fixed
-         // // This should trigger reconnection, which will now succeed.
-         // // We should be able to execute requests successfully now.
-         // co_await conn->async_exec(regular_req, ignore, net::redirect_error(ec));
-         // BOOST_TEST(ec == error_code());
+         // This should trigger reconnection, which will now succeed.
+         // We should be able to execute requests successfully now.
+         co_await conn->async_exec(regular_req, ignore, net::redirect_error(ec));
+         BOOST_TEST(ec == error_code());
       }
    }
 
