5263db7c54a6eaa9d35fec3c4bf8c92fd55ae96c
Ruben Perez <rubenperez038@gmail.com>
2025-06-15 13:00:41 +0200

    co_await guard

--- example/cpp20_unix_sockets.cpp
+++ example/cpp20_unix_sockets.cpp
@@ -5,9 +5,12 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <boost/asio/awaitable.hpp>
+
+#if defined(BOOST_ASIO_HAS_CO_AWAIT)
+
 #include <boost/redis/connection.hpp>
 
-#include <boost/asio/awaitable.hpp>
 #include <boost/asio/consign.hpp>
 #include <boost/asio/detached.hpp>
 #include <boost/asio/this_coro.hpp>
@@ -51,5 +54,7 @@ auto co_main(config) -> asio::awaitable<void>
    std::cout << "Sorry, your system does not support UNIX sockets\n";
    co_return;
 }
+
+#endif
 
 #endif
