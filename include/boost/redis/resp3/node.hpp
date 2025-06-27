5cdd0638118f25f41438a895983148b7fb67b07c
Nikolai Vladimirov <nikolai.vladimirov@cypherlabs.io>
2025-06-27 19:37:37 +0000

    Draft version without consume_one implementation

--- include/boost/redis/resp3/node.hpp
+++ include/boost/redis/resp3/node.hpp
@@ -59,6 +59,14 @@ using node = basic_node<std::string>;
 /// A node in the response tree that does not own its data.
 using node_view = basic_node<std::string_view>;
 
+/**
+ * TODO: documentation
+ */
+struct offset_node : node_view {
+   std::size_t offset{};
+   std::size_t size{};
+};
+
 }  // namespace boost::redis::resp3
 
 #endif  // BOOST_REDIS_RESP3_NODE_HPP
