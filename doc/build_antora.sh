67efd062ca8e49548e45fb9d3a1c7e662467dbdf
Ruben Perez <rubenperez038@gmail.com>
2025-06-30 22:27:12 +0200

    cleanup 1

--- doc/build_antora.sh
+++ doc/build_antora.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Copyright (c) 2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
 
 set -e
 
