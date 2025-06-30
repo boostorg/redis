b1fb15fc92d922b83a029db1452882a813c2c8a3
Ruben Perez <rubenperez038@gmail.com>
2025-06-30 22:14:38 +0200

    Use CMake rather than compile commands

--- doc/build_antora.sh
+++ doc/build_antora.sh
@@ -5,5 +5,9 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
+# Required by our CMake.
+# Prevents Antora from cloning Boost again
+export BOOST_SRC_DIR=$(realpath $SCRIPT_DIR/../../..)
+
 npm ci
-npx antora redis-playbook.yml
+npx antora --log-format=pretty redis-playbook.yml
