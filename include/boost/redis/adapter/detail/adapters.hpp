c1b5377f60d903955c794f4a906ba51726d7c472
Nikolai Vladimirov <nikolai.vladimirov@cypherlabs.io>
2025-07-05 19:31:23 +0000

    Defined done_fn_type in adapters.hpp

--- include/boost/redis/adapter/detail/adapters.hpp
+++ include/boost/redis/adapter/detail/adapters.hpp
@@ -139,14 +139,16 @@ void boost_redis_from_bulk(T& t, resp3::basic_node<String> const& node, system::
 
 //================================================
 
+using done_fn_type = std::function<void()>;
+
 template <typename T>
-auto prepare_done(T&) noexcept -> std::function<void()>
+auto prepare_done(T&) noexcept -> done_fn_type
 {
    return [] { };
 }
 
 template <typename T>
-auto prepare_done(generic_flat_response& resp) noexcept -> std::function<void()>
+auto prepare_done(generic_flat_response& resp) noexcept -> done_fn_type
 {
    return [resp]() mutable {
       if (resp.has_value()) {
