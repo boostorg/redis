beab3f69edb1c50548458a281cca5f0085751804
Anarthal (Rubén Pérez) <34971811+anarthal@users.noreply.github.com>
2025-09-27 18:13:02 +0200

    Reduces the time elapsed by test_conn_health_check and adds test_conn_monitor (#316)
    Splits test_conn_health_check into a test to verify that the health checker works and another to verify that MONITOR is handled correctly.
    Tests no longer wait or use CLIENT PAUSE

--- test/test_conn_check_health.cpp
+++ test/test_conn_check_health.cpp
@@ -5,16 +5,15 @@
  */
 
 #include <boost/redis/connection.hpp>
+#include <boost/redis/ignore.hpp>
 #include <boost/redis/response.hpp>
 
-#include <cstddef>
-#define BOOST_TEST_MODULE check_health
-#include <boost/test/included/unit_test.hpp>
+#include <boost/asio/error.hpp>
+#include <boost/core/lightweight_test.hpp>
 
 #include "common.hpp"
 
-#include <iostream>
-#include <thread>
+#include <cstddef>
 
 namespace net = boost::asio;
 namespace redis = boost::redis;
@@ -22,116 +21,65 @@ using error_code = boost::system::error_code;
 using connection = boost::redis::connection;
 using boost::redis::request;
 using boost::redis::ignore;
-using boost::redis::operation;
-using boost::redis::generic_response;
-using boost::redis::consume_one;
 using namespace std::chrono_literals;
-
-// TODO: Test cancel(health_check)
 
 namespace {
 
-struct push_callback {
-   connection* conn1;
-   connection* conn2;
-   generic_response* resp2;
-   request* req1;
-   int i = 0;
-   boost::asio::coroutine coro{};
-
-   void operator()(error_code ec = {}, std::size_t = 0)
-   {
-      BOOST_ASIO_CORO_REENTER(coro) for (;;)
-      {
-         BOOST_ASIO_CORO_YIELD
-         conn2->async_receive(*this);
-         if (ec) {
-            std::clog << "Exiting." << std::endl;
-            return;
-         }
-
-         BOOST_TEST(resp2->has_value());
-         BOOST_TEST(!resp2->value().empty());
-         std::clog << "Event> " << resp2->value().front().value << std::endl;
-         consume_one(*resp2);
-
-         ++i;
-
-         if (i == 5) {
-            std::clog << "Pausing the server" << std::endl;
-            // Pause the redis server to test if the health-check exits.
-            BOOST_ASIO_CORO_YIELD
-            conn1->async_exec(*req1, ignore, *this);
-            std::clog << "After pausing> " << ec.message() << std::endl;
-            // Don't know in CI we are getting: Got RESP3 simple-error.
-            //BOOST_TEST(!ec);
-            conn2->cancel(operation::run);
-            conn2->cancel(operation::receive);
-            conn2->cancel(operation::reconnection);
-            return;
-         }
-      }
-   };
-};
-
-BOOST_AUTO_TEST_CASE(check_health)
+void test_check_health()
 {
+   // Setup
    net::io_context ioc;
-   connection conn1{ioc};
+   connection conn{ioc};
 
+   // This request will block forever, causing the connection to become unresponsive
    request req1;
-   req1.push("CLIENT", "PAUSE", "10000", "ALL");
+   req1.push("BLPOP", "any", 0);
 
-   auto cfg1 = make_test_config();
-   cfg1.health_check_id = "conn1";
-   cfg1.reconnect_wait_interval = std::chrono::seconds::zero();
-
-   bool run1_finished = false, run2_finished = false, exec_finished = false;
-
-   conn1.async_run(cfg1, {}, [&](error_code ec) {
-      run1_finished = true;
-      std::cout << "async_run 1 completed: " << ec.message() << std::endl;
-      BOOST_TEST(ec != error_code());
-   });
-
-   //--------------------------------
-
-   // It looks like client pause does not work for clients that are
-   // sending MONITOR. I will therefore open a second connection.
-   connection conn2{ioc};
-
-   auto cfg2 = make_test_config();
-   cfg2.health_check_id = "conn2";
-   conn2.async_run(cfg2, {}, [&](error_code ec) {
-      run2_finished = true;
-      std::cout << "async_run 2 completed: " << ec.message() << std::endl;
-      BOOST_TEST(ec != error_code());
-   });
-
+   // This request should be executed after reconnection
    request req2;
-   req2.push("MONITOR");
-   generic_response resp2;
-   conn2.set_receive_response(resp2);
+   req2.push("PING", "after_reconnection");
+   req2.get_config().cancel_if_unresponded = false;
+   req2.get_config().cancel_on_connection_lost = false;
 
-   conn2.async_exec(req2, ignore, [&exec_finished](error_code ec, std::size_t) {
-      exec_finished = true;
-      std::cout << "async_exec: " << std::endl;
-      BOOST_TEST(ec == error_code());
+   // Make the test run faster
+   auto cfg = make_test_config();
+   cfg.health_check_interval = 500ms;
+   cfg.reconnect_wait_interval = 100ms;
+
+   bool run_finished = false, exec1_finished = false, exec2_finished = false;
+
+   conn.async_run(cfg, [&](error_code ec) {
+      run_finished = true;
+      BOOST_TEST_EQ(ec, net::error::operation_aborted);
    });
 
-   //--------------------------------
+   // This request will complete after the health checker deems the connection
+   // as unresponsive and triggers a reconnection (it's configured to be cancelled
+   // on connection lost).
+   conn.async_exec(req1, ignore, [&](error_code ec, std::size_t) {
+      exec1_finished = true;
+      BOOST_TEST_EQ(ec, net::error::operation_aborted);
 
-   push_callback{&conn1, &conn2, &resp2, &req1}();  // Starts reading pushes.
+      // Execute the second request. This one will succeed after reconnection
+      conn.async_exec(req2, ignore, [&](error_code ec2, std::size_t) {
+         exec2_finished = true;
+         BOOST_TEST_EQ(ec2, error_code());
+         conn.cancel();
+      });
+   });
 
-   ioc.run_for(2 * test_timeout);
+   ioc.run_for(test_timeout);
 
-   BOOST_TEST(run1_finished);
-   BOOST_TEST(run2_finished);
-   BOOST_TEST(exec_finished);
-
-   // Waits before exiting otherwise it might cause subsequent tests
-   // to fail.
-   std::this_thread::sleep_for(std::chrono::seconds{10});
+   BOOST_TEST(run_finished);
+   BOOST_TEST(exec1_finished);
+   BOOST_TEST(exec2_finished);
 }
 
 }  // namespace
+
+int main()
+{
+   test_check_health();
+
+   return boost::report_errors();
+}
