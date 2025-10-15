c27d4ea82c4d6af9854c4469a4b596862bc7e066
Ruben Perez <rubenperez038@gmail.com>
2025-10-15 13:15:18 +0200

    use the fixture

--- test/test_reader_fsm.cpp
+++ test/test_reader_fsm.cpp
@@ -5,6 +5,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <boost/redis/adapter/any_adapter.hpp>
 #include <boost/redis/detail/connection_state.hpp>
 #include <boost/redis/detail/reader_fsm.hpp>
 
@@ -78,14 +79,14 @@ void copy_to(multiplexer& mpx, std::string_view data)
 
 struct fixture : redis::detail::log_fixture {
    connection_state st{make_logger()};
+   generic_response resp;
+
+   fixture() { st.mpx.set_receive_adapter(any_adapter{resp}); }
 };
 
 void test_push()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -99,7 +100,7 @@ void test_push()
       ">1\r\n+msg2 \r\n"
       ">1\r\n+msg3  \r\n";
 
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
 
    // Deliver the 1st push
    act = fsm.resume(fix.st, payload.size(), ec, cancellation_type_t::none);
@@ -121,9 +122,6 @@ void test_push()
 void test_read_needs_more()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -136,18 +134,18 @@ void test_read_needs_more()
    std::string const msg[] = {">3\r", "\n+msg1\r\n+ms", "g2\r\n+msg3\r\n"};
 
    // Passes the first part to the fsm.
-   copy_to(mpx, msg[0]);
+   copy_to(fix.st.mpx, msg[0]);
    act = fsm.resume(fix.st, msg[0].size(), ec, cancellation_type_t::none);
    BOOST_TEST_EQ(act, action::type::read_some);
 
    // Passes the second part to the fsm.
-   copy_to(mpx, msg[1]);
+   copy_to(fix.st.mpx, msg[1]);
    act = fsm.resume(fix.st, msg[1].size(), ec, cancellation_type_t::none);
    BOOST_TEST_EQ(act, action::type::read_some);
 
    // Passes the third and last part to the fsm, next it should ask us
    // to deliver the message.
-   copy_to(mpx, msg[2]);
+   copy_to(fix.st.mpx, msg[2]);
    act = fsm.resume(fix.st, msg[2].size(), ec, cancellation_type_t::none);
    BOOST_TEST_EQ(act, action::notify_push_receiver(msg[0].size() + msg[1].size() + msg[2].size()));
 
@@ -159,9 +157,6 @@ void test_read_needs_more()
 void test_read_error()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -171,7 +166,7 @@ void test_read_error()
 
    // The fsm is asking for data.
    std::string const payload = ">1\r\n+msg1\r\n";
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
 
    // Deliver the data
    act = fsm.resume(
@@ -185,9 +180,6 @@ void test_read_error()
 void test_parse_error()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -197,7 +189,7 @@ void test_parse_error()
 
    // The fsm is asking for data.
    std::string const payload = ">a\r\n";
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
 
    // Deliver the data
    act = fsm.resume(fix.st, payload.size(), {}, cancellation_type_t::none);
@@ -207,9 +199,6 @@ void test_parse_error()
 void test_push_deliver_error()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -219,11 +208,11 @@ void test_push_deliver_error()
 
    // The fsm is asking for data.
    std::string const payload = ">1\r\n+msg1\r\n";
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
 
    // Deliver the data
    act = fsm.resume(fix.st, payload.size(), {}, cancellation_type_t::none);
-   BOOST_TEST_EQ(act, action::notify_push_receiver(4u));
+   BOOST_TEST_EQ(act, action::notify_push_receiver(11u));
 
    // Resumes from notifying a push with an error.
    act = fsm.resume(fix.st, 0, net::error::operation_aborted, cancellation_type_t::none);
@@ -237,10 +226,7 @@ void test_max_read_buffer_size()
    cfg.read_buffer_append_size = 5;
    cfg.max_read_size = 7;
 
-   multiplexer mpx;
-   mpx.set_config(cfg);
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
+   fix.st.mpx.set_config(cfg);
    reader_fsm fsm;
    error_code ec;
 
@@ -250,7 +236,7 @@ void test_max_read_buffer_size()
 
    // Passes the first part to the fsm.
    std::string const part1 = ">3\r\n";
-   copy_to(mpx, part1);
+   copy_to(fix.st.mpx, part1);
    act = fsm.resume(fix.st, part1.size(), {}, cancellation_type_t::none);
    BOOST_TEST_EQ(act, error_code(redis::error::exceeds_maximum_read_buffer_size));
 }
@@ -259,9 +245,6 @@ void test_max_read_buffer_size()
 void test_cancel_after_read()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -272,7 +255,7 @@ void test_cancel_after_read()
    // Deliver a push, and notify a cancellation.
    // This can happen if the cancellation signal arrives before the read handler runs
    constexpr std::string_view payload = ">1\r\n+msg1\r\n";
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
    act = fsm.resume(fix.st, payload.size(), ec, cancellation_type_t::terminal);
    BOOST_TEST_EQ(act, error_code(net::error::operation_aborted));
 }
@@ -280,9 +263,6 @@ void test_cancel_after_read()
 void test_cancel_after_push_delivery()
 {
    fixture fix;
-   multiplexer mpx;
-   generic_response resp;
-   mpx.set_receive_adapter(any_adapter{resp});
    reader_fsm fsm;
    error_code ec;
 
@@ -295,7 +275,7 @@ void test_cancel_after_push_delivery()
       ">1\r\n+msg1\r\n"
       ">1\r\n+msg2 \r\n";
 
-   copy_to(mpx, payload);
+   copy_to(fix.st.mpx, payload);
 
    // Deliver the 1st push
    act = fsm.resume(fix.st, payload.size(), ec, cancellation_type_t::none);
