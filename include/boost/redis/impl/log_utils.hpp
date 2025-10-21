87e1ff2509872a62c07269636251cf438983924a
Ruben Perez <rubenperez038@gmail.com>
2025-10-21 11:44:04 +0200

    Comments and refactor

--- include/boost/redis/impl/log_utils.hpp
+++ include/boost/redis/impl/log_utils.hpp
@@ -9,6 +9,7 @@
 
 #include <boost/redis/logger.hpp>
 
+#include <boost/core/ignore_unused.hpp>
 #include <boost/system/error_code.hpp>
 
 #include <cstddef>
@@ -17,11 +18,16 @@
 
 namespace boost::redis::detail {
 
+// Internal trait that defines how to log different types.
+// The base template applies to types convertible to string_view
 template <class T>
 struct log_traits {
+   // log should convert the input value to string and append it to the supplied buffer
    static inline void log(std::string& to, std::string_view value) { to += value; }
 };
 
+// Formatting size_t and error codes is shared between almost all FSMs, so it's defined here.
+// Support for types used only in one FSM should be added in the relevant FSM file.
 template <>
 struct log_traits<std::size_t> {
    static inline void log(std::string& to, std::size_t value) { to += std::to_string(value); }
@@ -41,20 +47,28 @@ struct log_traits<system::error_code> {
    }
 };
 
+// Logs a message with the specified severity to the logger.
+// Formatting won't be performed if the logger's level is inferior to lvl.
+// args are stringized using log_traits, and concatenated.
 template <class... Args>
 void log(buffered_logger& to, logger::level lvl, const Args&... args)
 {
+   // Severity check
    if (to.lgr.lvl < lvl)
       return;
 
+   // Clear the buffer
    to.buffer.clear();
 
-   auto a = {(log_traits<Args>::log(to.buffer, args), 0)...};
-   static_cast<void>(a);
+   // Format all arguments
+   auto dummy = {(log_traits<Args>::log(to.buffer, args), 0)...};
+   ignore_unused(dummy);
 
+   // Invoke the function
    to.lgr.fn(lvl, to.buffer);
 }
 
+// Shorthand for each log level we use
 template <class... Args>
 void log_debug(buffered_logger& to, const Args&... args)
 {
