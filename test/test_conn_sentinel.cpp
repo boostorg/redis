de8c2f6882179769d72093dec2cf97c249feeab4
Ruben Perez <rubenperez038@gmail.com>
2025-10-30 18:09:44 +0100

    Initial test

---
+++ test/test_conn_sentinel.cpp
@@ -0,0 +1,72 @@
+//
+// Copyright (c) 2025 Marcelo Zimbres Silva (mzimbres@gmail.com),
+// Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/redis/connection.hpp>
+#include <boost/redis/ignore.hpp>
+
+#include <boost/core/lightweight_test.hpp>
+
+#include "boost/redis/config.hpp"
+#include "boost/redis/request.hpp"
+#include "boost/redis/response.hpp"
+#include "common.hpp"
+
+namespace net = boost::asio;
+using namespace boost::redis;
+using namespace std::chrono_literals;
+using boost::system::error_code;
+
+namespace {
+
+void test_exec()
+{
+   // Setup
+   net::io_context ioc;
+   connection conn{ioc};
+
+   config cfg;
+   cfg.sentinel.addresses = {
+      {"localhost", "26379"},
+      {"localhost", "26380"},
+      {"localhost", "26381"},
+   };
+   cfg.sentinel.master_name = "mymaster";
+
+   request req;
+   req.push("INFO", "server");
+
+   response<std::string> resp;
+
+   bool exec_finished = false, run_finished = false;
+
+   conn.async_exec(req, resp, [&](error_code ec, std::size_t) {
+      exec_finished = true;
+      BOOST_TEST_EQ(ec, error_code());
+      BOOST_TEST_EQ(find_client_info(std::get<0>(resp).value(), "laddr"), "127.0.0.1:6380");
+      conn.cancel();
+   });
+
+   conn.async_run(cfg, [&](error_code ec) {
+      run_finished = true;
+      BOOST_TEST_EQ(ec, net::error::operation_aborted);
+   });
+
+   ioc.run_for(test_timeout);
+
+   BOOST_TEST(exec_finished);
+   BOOST_TEST(run_finished);
+}
+
+}  // namespace
+
+int main()
+{
+   test_exec();
+
+   return boost::report_errors();
+}
