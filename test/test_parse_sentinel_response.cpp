551e3bb929061737b13b425252d4c3dce1e07e3b
Ruben Perez <rubenperez038@gmail.com>
2025-11-11 18:22:57 +0100

    replica test

--- test/test_parse_sentinel_response.cpp
+++ test/test_parse_sentinel_response.cpp
@@ -234,6 +234,84 @@ void test_master_ip_port_out_of_order()
    BOOST_TEST_EQ(resp.replicas.size(), 0u);
 }
 
+// Usual response when asking for a replica
+void test_replica()
+{
+   // Setup
+   sentinel_response resp{
+      "leftover",
+      {"leftover_host", "6543"},
+      {address()},
+      {address()},
+   };
+
+   auto nodes = from_resp3({
+      // clang-format off
+      "*2\r\n$9\r\nlocalhost\r\n$4\r\n6380\r\n",
+
+      "*2\r\n"
+         "%21\r\n"
+            "$4\r\nname\r\n$14\r\nlocalhost:6381\r\n$2\r\nip\r\n$9\r\nsome.host\r\n$4\r\nport\r\n$4\r\n6381\r\n"
+            "$5\r\nrunid\r\n$40\r\ncdfa33e2d39958c0b10c0391c0c3d4ab096edfeb\r\n$5\r\nflags\r\n$5\r\nslave\r\n"
+            "$21\r\nlink-pending-commands\r\n$1\r\n0\r\n$13\r\nlink-refcount\r\n$1\r\n1\r\n$14\r\nlast-ping-sent\r\n$1\r\n0\r\n"
+            "$18\r\nlast-ok-ping-reply\r\n$3\r\n134\r\n$15\r\nlast-ping-reply\r\n$3\r\n134\r\n$23\r\ndown-after-milliseconds\r\n$5\r\n10000\r\n"
+            "$12\r\ninfo-refresh\r\n$4\r\n5302\r\n$13\r\nrole-reported\r\n$5\r\nslave\r\n$18\r\nrole-reported-time\r\n$6\r\n442121\r\n"
+            "$21\r\nmaster-link-down-time\r\n$1\r\n0\r\n$18\r\nmaster-link-status\r\n$2\r\nok\r\n$11\r\nmaster-host\r\n$9\r\nlocalhost\r\n"
+            "$11\r\nmaster-port\r\n$4\r\n6380\r\n$14\r\nslave-priority\r\n$3\r\n100\r\n$17\r\nslave-repl-offset\r\n$5\r\n29110\r\n"
+            "$17\r\nreplica-announced\r\n$1\r\n1\r\n"
+         "%21\r\n"
+            "$4\r\nname\r\n$14\r\nlocalhost:6382\r\n$2\r\nip\r\n$9\r\ntest.host\r\n$4\r\nport\r\n$4\r\n6382\r\n"
+            "$5\r\nrunid\r\n$40\r\n11bfea62c25316e211fdf0e1ccd2dbd920e90815\r\n$5\r\nflags\r\n$5\r\nslave\r\n"
+            "$21\r\nlink-pending-commands\r\n$1\r\n0\r\n$13\r\nlink-refcount\r\n$1\r\n1\r\n$14\r\nlast-ping-sent\r\n$1\r\n0\r\n"
+            "$18\r\nlast-ok-ping-reply\r\n$3\r\n134\r\n$15\r\nlast-ping-reply\r\n$3\r\n134\r\n$23\r\ndown-after-milliseconds\r\n$5\r\n10000\r\n"
+            "$12\r\ninfo-refresh\r\n$4\r\n5302\r\n$13\r\nrole-reported\r\n$5\r\nslave\r\n$18\r\nrole-reported-time\r\n$6\r\n442132\r\n"
+            "$21\r\nmaster-link-down-time\r\n$1\r\n0\r\n$18\r\nmaster-link-status\r\n$2\r\nok\r\n$11\r\nmaster-host\r\n$9\r\nlocalhost\r\n"
+            "$11\r\nmaster-port\r\n$4\r\n6380\r\n$14\r\nslave-priority\r\n$3\r\n100\r\n$17\r\nslave-repl-offset\r\n$5\r\n29110\r\n"
+            "$17\r\nreplica-announced\r\n$1\r\n1\r\n",
+
+      "*2\r\n"
+         "%14\r\n"
+            "$4\r\nname\r\n$40\r\nf14ef06a8a478cdd66ded467ec18accd2a24b731\r\n$2\r\nip\r\n$8\r\nhost.one\r\n$4\r\nport\r\n$5\r\n26380\r\n"
+            "$5\r\nrunid\r\n$40\r\nf14ef06a8a478cdd66ded467ec18accd2a24b731\r\n$5\r\nflags\r\n$8\r\nsentinel\r\n"
+            "$21\r\nlink-pending-commands\r\n$1\r\n0\r\n$13\r\nlink-refcount\r\n$1\r\n1\r\n$14\r\nlast-ping-sent\r\n$1\r\n0\r\n"
+            "$18\r\nlast-ok-ping-reply\r\n$3\r\n696\r\n$15\r\nlast-ping-reply\r\n$3\r\n696\r\n$23\r\ndown-after-milliseconds\r\n$5\r\n10000\r\n"
+            "$18\r\nlast-hello-message\r\n$3\r\n334\r\n$12\r\nvoted-leader\r\n$1\r\n?\r\n$18\r\nvoted-leader-epoch\r\n$1\r\n0\r\n"
+         "%14\r\n"
+            "$4\r\nname\r\n$40\r\nf9b54e79e2e7d3f17ad60527504191ec8a861f27\r\n$2\r\nip\r\n$8\r\nhost.two\r\n$4\r\nport\r\n$5\r\n26381\r\n"
+            "$5\r\nrunid\r\n$40\r\nf9b54e79e2e7d3f17ad60527504191ec8a861f27\r\n$5\r\nflags\r\n$8\r\nsentinel\r\n"
+            "$21\r\nlink-pending-commands\r\n$1\r\n0\r\n$13\r\nlink-refcount\r\n$1\r\n1\r\n$14\r\nlast-ping-sent\r\n$1\r\n0\r\n"
+            "$18\r\nlast-ok-ping-reply\r\n$3\r\n696\r\n$15\r\nlast-ping-reply\r\n$3\r\n696\r\n$23\r\ndown-after-milliseconds\r\n$5\r\n10000\r\n"
+            "$18\r\nlast-hello-message\r\n$3\r\n134\r\n$12\r\nvoted-leader\r\n$1\r\n?\r\n$18\r\nvoted-leader-epoch\r\n$1\r\n0\r\n",
+      // clang-format on
+   });
+
+   // Call the function
+   auto ec = parse_sentinel_response(nodes, 3u, role::replica, resp);
+   BOOST_TEST_EQ(ec, error_code());
+
+   // Check
+   const address expected_sentinels[] = {
+      {"host.one", "26380"},
+      {"host.two", "26381"},
+   };
+   const address expected_replicas[] = {
+      {"some.host", "6381"},
+      {"test.host", "6382"},
+   };
+   BOOST_TEST_EQ(resp.diagnostic, "");
+   BOOST_TEST_EQ(resp.master_addr, (address{"localhost", "6380"}));
+   BOOST_TEST_ALL_EQ(
+      resp.sentinels.begin(),
+      resp.sentinels.end(),
+      std::begin(expected_sentinels),
+      std::end(expected_sentinels));
+   BOOST_TEST_ALL_EQ(
+      resp.replicas.begin(),
+      resp.replicas.end(),
+      std::begin(expected_replicas),
+      std::end(expected_replicas));
+}
+
 }  // namespace
 
 int main()
@@ -242,6 +320,8 @@ int main()
    test_master_no_sentinels();
    test_master_setup_request();
    test_master_ip_port_out_of_order();
+
+   test_replica();
 
    return boost::report_errors();
 }
