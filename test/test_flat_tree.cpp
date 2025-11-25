82a3f8b810a6f9a2040f566d53262d2e2904906d
Ruben Perez <rubenperez038@gmail.com>
2025-11-25 12:55:45 +0100

    cleanup

--- test/test_flat_tree.cpp
+++ test/test_flat_tree.cpp
@@ -41,34 +41,6 @@ namespace {
 constexpr std::string_view
    resp3_set = "~6\r\n+orange\r\n+apple\r\n+one\r\n+two\r\n+three\r\n+orange\r\n";
 
-node from_node_view(node_view const& v)
-{
-   node ret;
-   ret.data_type = v.data_type;
-   ret.aggregate_size = v.aggregate_size;
-   ret.depth = v.depth;
-   ret.value = v.value;
-   return ret;
-}
-
-tree from_flat(generic_flat_response const& resp)
-{
-   tree ret;
-   for (auto const& e : resp.value().get_view())
-      ret.push_back(from_node_view(e));
-
-   return ret;
-}
-
-tree from_flat(flat_tree const& resp)
-{
-   tree ret;
-   for (auto const& e : resp.get_view())
-      ret.push_back(from_node_view(e));
-
-   return ret;
-}
-
 void add_nodes(
    flat_tree& to,
    std::string_view data,
@@ -823,38 +795,8 @@ void test_comparison_self()
    BOOST_TEST_NOT(tempty != tempty);
 }
 
-// Parses the same data into a tree and a
-// flat_tree, they should be equal to each other.
-void test_views_are_set()
-{
-   tree resp1;
-   flat_tree resp2;
-   generic_flat_response resp3;
-
-   error_code ec;
-   deserialize(resp3_set, adapt2(resp1), ec);
-   BOOST_TEST_EQ(ec, error_code{});
-
-   deserialize(resp3_set, adapt2(resp2), ec);
-   BOOST_TEST_EQ(ec, error_code{});
-
-   deserialize(resp3_set, adapt2(resp3), ec);
-   BOOST_TEST_EQ(ec, error_code{});
-
-   BOOST_TEST_EQ(resp2.get_reallocs(), 1u);
-   BOOST_TEST_EQ(resp2.get_total_msgs(), 1u);
-
-   BOOST_TEST_EQ(resp3.value().get_reallocs(), 1u);
-   BOOST_TEST_EQ(resp3.value().get_total_msgs(), 1u);
-
-   auto const tmp2 = from_flat(resp2);
-   BOOST_TEST_ALL_EQ(resp1.begin(), resp1.end(), tmp2.begin(), tmp2.end());
-
-   auto const tmp3 = from_flat(resp3);
-   BOOST_TEST_ALL_EQ(resp1.begin(), resp1.end(), tmp3.begin(), tmp3.end());
-}
-
 // The response should be reusable.
+// TODO: this belongs to clear
 void test_reuse()
 {
    flat_tree tmp;
@@ -880,49 +822,6 @@ void test_reuse()
    BOOST_TEST_EQ(tmp.get_total_msgs(), 1u);
 
    BOOST_TEST_ALL_EQ(resp1.begin(), resp1.end(), tmp.get_view().begin(), tmp.get_view().end());
-}
-
-void test_copy_assign_2()
-{
-   flat_tree ref1;
-   flat_tree ref2;
-   flat_tree ref3;
-   flat_tree ref4;
-
-   error_code ec;
-   deserialize(resp3_set, adapt2(ref1), ec);
-   deserialize(resp3_set, adapt2(ref2), ec);
-   deserialize(resp3_set, adapt2(ref3), ec);
-   deserialize(resp3_set, adapt2(ref4), ec);
-   BOOST_TEST_EQ(ec, error_code{});
-
-   // Copy ctor
-   flat_tree copy1{ref1};
-
-   // Move ctor
-   flat_tree move1{std::move(ref2)};
-
-   // Copy assignment
-   flat_tree copy2 = ref1;
-
-   // Move assignment
-   flat_tree move2 = std::move(ref3);
-
-   // Assignment
-   flat_tree copy3;
-   copy3 = ref1;
-
-   // Move assignment
-   flat_tree move3;
-   move3 = std::move(ref4);
-
-   BOOST_TEST((copy1 == ref1));
-   BOOST_TEST((copy2 == ref1));
-   BOOST_TEST((copy3 == ref1));
-
-   BOOST_TEST((move1 == ref1));
-   BOOST_TEST((move2 == ref1));
-   BOOST_TEST((move3 == ref1));
 }
 
 }  // namespace
@@ -975,8 +874,6 @@ int main()
    test_comparison_empty();
    test_comparison_self();
 
-   test_views_are_set();
-   test_copy_assign_2();
    test_reuse();
 
    return boost::report_errors();
