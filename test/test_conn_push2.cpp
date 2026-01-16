17441eb8e6cbda1de24f79ab243b651e5f853f5b
Ruben Perez <rubenperez038@gmail.com>
2026-01-16 13:06:23 +0100

    batch test

--- test/test_conn_push2.cpp
+++ test/test_conn_push2.cpp
@@ -12,6 +12,7 @@
 #include <boost/redis/resp3/flat_tree.hpp>
 #include <boost/redis/response.hpp>
 
+#include <boost/asio/cancel_after.hpp>
 #include <boost/asio/error.hpp>
 #include <boost/asio/experimental/channel_error.hpp>
 #include <boost/core/lightweight_test.hpp>
@@ -128,7 +129,72 @@ void test_async_receive2_push_available()
    BOOST_TEST(run_finished);
 }
 
-// TODO: several pushes available
+// async_receive2 blocks only once if several messages are received in a batch
+void test_async_receive2_batch()
+{
+   struct impl {
+      net::io_context ioc{};
+      connection conn{ioc};
+      resp3::flat_tree resp{};
+      request req{};
+      bool receive_finished = false, run_finished = false;
+
+      // Trigger pushes
+      void start_exec()
+      {
+         conn.async_exec(req, ignore, [this](error_code ec, std::size_t) {
+            BOOST_TEST_EQ(ec, error_code());
+            start_receive1();
+         });
+      }
+
+      // Receive the two pushes
+      void start_receive1()
+      {
+         conn.async_receive2([this](error_code ec) {
+            BOOST_TEST_EQ(ec, error_code());
+            BOOST_TEST_EQ(resp.get_total_msgs(), 2u);
+            start_receive2();
+         });
+      }
+
+      // The previous receive has consumed the two pushes,
+      // so this one will block (and be cancelled).
+      void start_receive2()
+      {
+         conn.async_receive2(net::cancel_after(50ms, [this](error_code ec) {
+            BOOST_TEST_EQ(ec, net::error::operation_aborted);
+            receive_finished = true;
+            conn.cancel();
+         }));
+      }
+
+      void run()
+      {
+         // Setup
+         conn.set_receive_response(resp);
+
+         // Cause two messages to be delivered. The PING ensures that
+         // the pushes have been read when exec completes
+         req.push("SUBSCRIBE", "test_async_receive2_batch");
+         req.push("SUBSCRIBE", "test_async_receive2_batch");
+         req.push("PING", "message");
+
+         start_exec();
+         conn.async_run(make_test_config(), [&](error_code ec) {
+            run_finished = true;
+            BOOST_TEST_EQ(ec, net::error::operation_aborted);
+         });
+
+         ioc.run_for(test_timeout);
+
+         BOOST_TEST(receive_finished);
+         BOOST_TEST(run_finished);
+      }
+   };
+
+   impl{}.run();
+}
 
 // async_receive2 can be called several times in a row
 void test_async_receive2_subsequent_calls()
@@ -677,6 +743,7 @@ int main()
 {
    test_async_receive2_waiting_for_push();
    test_async_receive2_push_available();
+   test_async_receive2_batch();
    test_async_receive2_subsequent_calls();
    test_exec_push_interleaved();
    test_push_adapter_error();
