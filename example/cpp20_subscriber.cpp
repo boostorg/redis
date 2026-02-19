21dff1d03da73b604b0ad2a32c7a88928f326eaf
Ruben Perez <rubenperez038@gmail.com>
2026-02-19 21:20:48 +0100

    pushes, schematic page

--- example/cpp20_subscriber.cpp
+++ example/cpp20_subscriber.cpp
@@ -73,6 +73,13 @@ auto receiver(std::shared_ptr<connection> conn) -> asio::awaitable<void>
          break;
       }
 
+      // This can happen if a SUBSCRIBE command errored (e.g. insufficient permissions)
+      if (resp.has_error()) {
+         std::cerr << "The receive response contains an error: " << resp.error().diagnostic
+                   << std::endl;
+         break;
+      }
+
       // The response must be consumed without suspending the
       // coroutine i.e. without the use of async operations.
       for (push_view elem : push_parser(resp.value())) {
