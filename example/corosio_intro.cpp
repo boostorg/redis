1524652b5aa80c044995fb323b066b6ac9025d2c
Ruben Perez <rubenperez038@gmail.com>
2026-04-21 11:08:20 +0200

    cleanup example

--- example/corosio_intro.cpp
+++ example/corosio_intro.cpp
@@ -16,13 +16,11 @@
 #include <exception>
 #include <iostream>
 
-// TODO: re-add this to CMake!
-
 namespace capy = boost::capy;
 using namespace boost::redis;
 namespace corosio = boost::corosio;
 
-capy::task<void> run_request(co_connection& conn)
+capy::io_task<> run_request(co_connection& conn)
 {
    // A request containing only a ping command.
    request req;
@@ -33,33 +31,47 @@ capy::task<void> run_request(co_connection& conn)
 
    // Executes the request.
    auto [ec] = co_await conn.exec(req, resp);
-   if (ec)
-      co_return;
-   std::cout << "PING value: " << std::get<0>(resp).value() << std::endl;
+   if (ec) {
+      std::cout << "Error executing PING: " << ec << std::endl;
+   } else {
+      std::cout << "PING value: " << std::get<0>(resp).value() << std::endl;
+   }
+
+   co_return {};
 }
 
 capy::task<void> co_main()
 {
    // Create a connection
-   co_connection conn{(co_await capy::this_coro::executor).context()};
+   co_connection conn{co_await capy::this_coro::executor};
 
-   auto r = co_await capy::when_any(run_request(conn), conn.run(config{}));
-
-   static_cast<void>(r);
+   // Run the connection and the PING request, in parallel
+   co_await capy::when_any(run_request(conn), conn.run(config{}));
 }
-
-struct handler {
-   void operator()() { std::cout << "Done\n"; }
-   void operator()(std::exception_ptr exc)
-   {
-      if (exc)
-         std::rethrow_exception(exc);
-   }
-};
 
 int main()
 {
+   // The I/O context, required for all I/O operations
    corosio::io_context ctx;
-   capy::run_async(ctx.get_executor())(co_main());
+
+   // Schedules the main coroutine for execution
+   capy::run_async(
+      ctx.get_executor(),
+      []() {
+         // Runs when the main coroutine finishes normally
+         std::cout << "Done\n";
+      },
+      [](std::exception_ptr exc) {
+         // Runs when the main coroutine finishes with an exception
+         try {
+            std::rethrow_exception(exc);
+         } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            exit(1);
+         }
+         exit(1);
+      })(co_main());
+
+   // Executes all pending work, including the main coroutine
    ctx.run();
 }
